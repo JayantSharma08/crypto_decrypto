19d9c41e6e35adff16d2fac68030f0b312f849ff
Paulescu <plabartabajo@gmail.com>
2024-12-16 12:06:11 +0100

    Add golden dataset generation script
    I also added some sample data the script generated in a few minutes.

--- services/news-signal/llms/base.py
+++ services/news-signal/llms/base.py
@@ -31,11 +31,12 @@ class NewsSignalOneCoin(BaseModel):
         'ZEC',
         'ETC',
     ] = Field(description='The coin that the news is about')
-    signal: Literal[1, -1] = Field(
+    signal: Literal[1, 0, -1] = Field(
         description="""
     The signal of the news on the coin price.
     1 if the price is expected to go up
     -1 if it is expected to go down.
+    0 if the news is not related to the coin.
 
     If the news is not related to the coin, no need to create a NewsSignal.
     """
